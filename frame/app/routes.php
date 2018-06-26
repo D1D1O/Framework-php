b3fa302073f90c73d4f740a76a2a1678d1c2c2af
D1D1O <didiosilva395@gmail.com>
2018-06-26 00:08:11 -0400

    esta sendo configurado as rotas obetendo rotas do navegador e comparando com as rotas que temos no nosso sitema.

--- frame/app/routes.php
+++ frame/app/routes.php
@@ -1,6 +1,9 @@
 <?php 
 
 
-$route[] = ['/','HomeController@index'];
+	$route[] = ['/'         ,  'HomeController@index'];
+	$route[] = ['/post'     ,  'PostController@index'];
+	$route[] = ['/post/{id}/show',  'PostController@show' ];
+
 
 return $route;
--- frame/core/Route.php
+++ frame/core/Route.php
@@ -19,8 +19,30 @@ class Route{
 		return parse_url($_SERVER['REQUEST_URI'],PHP_URL_PATH);
 	}
 	private function run(){
-		echo $this->getUrl();
-	}
+		$url = $this->getUrl();
+		$urlArray = explode('/', $url);
+		print_r($urlArray);
+		
+		foreach ($this->routes as $route) {
+			$routeArray = explode('/',$route[0]);
+			echo '<br>';
+			print_r($routeArray);
 
+			for ($i=0; $i < count($routeArray) ; $i++) {
+			
+				if ((strpos($routeArray[$i], "{") !==false) && (count($urlArray)== count($routeArray))) {
+					$routeArray[$i] = $urlArray[$i];
+				}
 
+				$route[0] = implode($routeArray, '/');
+			}
+			if ($url == $route[0]) {
+
+				echo '<br>Rota valida';
+			}else{
+				echo '<br>Rota invalida';
+			}
+
+		}
+	} 
 }
